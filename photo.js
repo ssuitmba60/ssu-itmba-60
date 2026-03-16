d30a48e2e28b0ce6fc157958cba4b303cd973aa1
박규태 <satcop@naver.com>
2026-03-17 00:25:59 +0900

    [포토추가]

--- photo.js
+++ photo.js
@@ -1,28 +1,53 @@
 const gallery = document.getElementById("gallery");
-const images = gallery.querySelectorAll("img");
+const images = Array.from(gallery.querySelectorAll("img"));
 
 let loaded = 0;
 const total = images.length;
-const percent = Math.floor((loaded / total) * 100);
+
 const loadingCount = document.getElementById("loading-count");
-//loadingCount.textContent = `0 / ${total}`;
-loadingCount.textContent = `${loaded} / ${total} (${percent}%)`;
+loadingCount.textContent = `0 / ${total}`;
 
 const showAfter = Math.max(5, total * 0.3);
 
-images.forEach(img => {
+// 동시에 로딩할 개수
+const CONCURRENT = 10;
+
+let index = 0;
+
+function loadNext() {
+
+    if (index >= total) return;
+
+    const img = images[index];
     const temp = new Image();
+
     temp.src = img.src;
 
     temp.onload = () => {
+
         loaded++;
 
-        // 로딩 숫자 표시
-        loadingCount.textContent = `${loaded} / ${total}`;
+        const percent = Math.floor((loaded / total) * 100);
+        loadingCount.textContent = `${loaded} / ${total} (${percent}%)`;
 
         if (loaded >= showAfter) {
             document.getElementById("loading").style.display = "none";
             gallery.style.display = "grid";
         }
-    }
+
+        loadNext();
+    };
+
+    index++;
+}
+
+// 최초 병렬 로딩 시작
+for (let i = 0; i < CONCURRENT; i++) {
+    loadNext();
+}
+
+lightGallery(gallery, {
+    speed: 300,
+    download: true,
+    zoom: true
 });
